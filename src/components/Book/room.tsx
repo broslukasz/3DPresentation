2d936cbe66776ebf89775eb94cdea82ac70fe8c0
Lukasz Bros <lbros@adnoc.ae>
2024-11-21 13:07:04 +0100

    play animations

--- src/components/Book/room.tsx
+++ src/components/Book/room.tsx
@@ -1,20 +1,33 @@
+import { useAnimations } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 export default function Book({ onDoubleClick, onClick }) {
-  const gltf = useMemo(() => useLoader(GLTFLoader, 'src/assets/medival_book.glb',), []);
-  const model = gltf.scene;
+  const group = useRef();
+  const { scene, animations } = useMemo(() => useLoader(GLTFLoader, 'src/assets/medival_book.glb',), []);
+  const { actions } = useAnimations(animations, group);
+
+  useEffect(() => {
+    if (actions && actions[Object.keys(actions)[0]]) {
+      actions[Object.keys(actions)[0]].play();
+    }
+  }, [actions]);
+
 
   return <>
+  <group 
+    ref={group}
+    dispose={null}
+    onDoubleClick={onDoubleClick}
+    onClick={onClick}>
     <primitive
       castShadow
       receiveShadow
-      onDoubleClick={onDoubleClick}
-      onClick={onClick}
-      object={model}
+      object={scene}
       scale={0.2}
      />
+  </group>
 
   </>
 }
