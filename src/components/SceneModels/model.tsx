05362d0aff86e24441edae0b7c0a427910e3a1c6
Lukasz Bros <lbros@adnoc.ae>
2024-12-17 09:51:10 +0100

    adding sample model

--- src/components/SceneModels/model.tsx
+++ src/components/SceneModels/model.tsx
@@ -7,6 +7,9 @@ export default function Model({ onDoubleClick, onClick }) {
   const group = useRef();
   const { scene, animations } = useMemo(() => useLoader(GLTFLoader, 'src/assets/medival_book.glb',), []);
   const { actions } = useAnimations(animations, group);
+  
+  // New model example
+  // const { scene: pineTreeScene } = useMemo(() => useLoader(GLTFLoader, 'src/assets/pine_tree.glb',), []);
 
   useEffect(() => {
     if (actions && actions[Object.keys(actions)[0]]) {
@@ -28,6 +31,15 @@ export default function Model({ onDoubleClick, onClick }) {
       scale={0.2}
      />
   </group>
+
+  {/* New Object example */}
+  {/* <primitive
+    castShadow
+    receiveShadow
+    object={pineTreeScene}
+    scale={0.02}
+    position={[0, 0, 0]}
+  /> */}
 
   </>
 }
